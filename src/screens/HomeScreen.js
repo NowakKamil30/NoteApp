a8de6c31067d757ba9b3002ecf72f8e6003423b8
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-22 11:34:29 +0100

    edit homeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,74 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import NoteElement from "../components/NoteElement";
+import RadiusButton from "../components/RadiusButton";
+import { _retrieveData, _storeData } from "../helpers/useData";
 
-export default HomeScreen = () => {
+HomeScreen = ({ navigation }) => {
+  const [data, useData] = useState([]);
+  useEffect(() => {}, []);
   return (
-    <View>
-      <Text>main</Text>
+    <View style={styles.mainView}>
+      <RadiusButton
+        onClick={() => {
+          let currentid;
+          _retrieveData("id")
+            .then(id => {
+              console.log(id);
+              if (id === "undefined") {
+                currentid = 1;
+              } else {
+                currentid = Number(id) + 1;
+              }
+              return _storeData("id", currentid);
+            })
+            .then(result => {
+              console.log(currentid);
+              useData([
+                ...data,
+                {
+                  shortText: "sacascasvasvfasdfefasfeaf",
+                  id: currentid,
+                  color: "green"
+                }
+              ]);
+              //return _retrieveData("usingID");
+            })
+            //.then(result => {})
+            .catch(err => {
+              console.log(err);
+            });
+        }}
+      />
+      <FlatList
+        style={styles.list}
+        data={data}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <NoteElement
+            onClick={() => {
+              navigation.navigate("Note", {
+                text: item.shortText,
+                id: item.id
+              });
+            }}
+            text={item.shortText}
+          />
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: "#AFB42B",
+    height: "100%",
+    width: "100%"
+  },
+  list: {
+    zIndex: 0
+  }
+});
+
+export default HomeScreen;
