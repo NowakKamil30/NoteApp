8ee4233c09b8258b2ea2070d4cc6ad853ae4525e
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-22 11:34:10 +0100

    use reeact navigation

--- App.js
+++ App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import NoteScreen from "./src/screens/NoteScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Note: NoteScreen
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "Mój notatnik"
+      title: "Mój notatnik",
+      headerStyle: {
+        backgroundColor: "#CDDC39"
+      }
     }
   }
 );
