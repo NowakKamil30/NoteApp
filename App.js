56f3646a3fba4aac7df14c851e5f973778d12f9a
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-23 23:54:32 +0100

    create provider and middleware for thunk

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 import HomeScreen from "./src/screens/HomeScreen";
 import NoteScreen from "./src/screens/NoteScreen";
+import reducers from "./src/reducers";
 
 const navigator = createStackNavigator(
   {
@@ -20,4 +24,13 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const Navigator = createAppContainer(navigator);
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
+};
+export default App;
