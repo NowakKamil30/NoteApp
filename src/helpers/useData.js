10db17d3f2c0a4e68fbf8f85ae0db058611d067f
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-28 23:33:17 +0100

    add createArrayNote helper function

--- src/helpers/useData.js
+++ src/helpers/useData.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from "react-native";
-
+import { colors } from "../../setting.json";
 export const _storeData = async (key, value) => {
   try {
     await AsyncStorage.setItem(String(key), String(value));
@@ -19,5 +19,31 @@ export const _retrieveData = async key => {
   }
 };
 export const getIdKeys = keys => {
-  while (true) {}
+  const keysArray = keys.split(";");
+  keysArray.pop();
+  return keysArray;
+};
+
+export const createArrayNotes = ids => {
+  return Promise.all(
+    ids.map(id => {
+      let noteColor;
+      return _retrieveData(`color-${id}`)
+        .then(color => {
+          const isCorrectColor = colors.filter(settingColor => {
+            return settingColor === color;
+          });
+          if (isCorrectColor) {
+            noteColor = color;
+          } else noteColor = colors[0];
+          return _retrieveData(`text-${id}`);
+        })
+        .then(text => {
+          if (text == "undefined" || text == undefined) {
+            text = "";
+          }
+          return { text, color: noteColor, id };
+        });
+    })
+  );
 };
