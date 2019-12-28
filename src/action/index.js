b5ab9b2ccb335eb4d2e41661067582d431446cbc
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-28 23:34:54 +0100

    action downloadNotes() send notes' data to reducers

--- src/action/index.js
+++ src/action/index.js
@@ -5,7 +5,26 @@ import {
   DOWNLOAD_ID,
   TEXT_NOTE
 } from "./types";
-import { _retrieveData, _storeData } from "../helpers/useData";
+import {
+  _retrieveData,
+  _storeData,
+  getIdKeys,
+  createArrayNotes
+} from "../helpers/useData";
+
+export const downloadNotes = () => {
+  return dispatch =>
+    _retrieveData("usingID")
+      .then(usingID => {
+        return getIdKeys(usingID);
+      })
+      .then(keyArray => {
+        return createArrayNotes(keyArray);
+      })
+      .then(keyArray => {
+        dispatch({ type: DOWNLOAD_NOTES, payload: keyArray });
+      });
+};
 
 export const addNote = () => {
   let currentid;
@@ -20,10 +39,22 @@ export const addNote = () => {
         return _storeData("id", currentid);
       })
       .then(result => {
-        dispatch({ type: DOWNLOAD_ID, payload: { currentid, error: false } });
-        //return _retrieveData("usingID");
+        return _retrieveData("usingID");
       })
-      //.then(result => {})
+      .then(usingID => {
+        let newUsingID = usingID;
+        if (newUsingID === "undefined" || newUsingID === undefined) {
+          newUsingID = "";
+        }
+        newUsingID += currentid + ";";
+        return _storeData("usingID", newUsingID);
+      })
+      .then(result => {
+        return _storeData(`color-${currentid}`, "green");
+      })
+      .then(result => {
+        dispatch({ type: DOWNLOAD_ID, payload: { currentid, error: false } });
+      })
       .catch(err => {
         console.log(err);
         dispatch({ type: DOWNLOAD_ID, payload: { currentid, error: true } });
@@ -31,10 +62,7 @@ export const addNote = () => {
 };
 
 export const textNote = (id, text) => {
-  console.log("work");
-
   return { type: TEXT_NOTE, payload: { id, text } };
 };
 
 export const deleteNote = id => ({ type: DELETE_NOTE, payload: id });
-export const downloadNotes = () => {};
