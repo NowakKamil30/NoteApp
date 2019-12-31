b114cf3c71353ec22696e8dabaa662d129c02c8e
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-31 03:02:43 +0100

    new actions

--- src/action/types.js
+++ src/action/types.js
@@ -3,3 +3,5 @@ export const DELETE_NOTE = "delete_note";
 export const DOWNLOAD_NOTES = "download_notes";
 export const DOWNLOAD_ID = "download_id";
 export const TEXT_NOTE = "text_note";
+export const OPEN_MODAL_NOTE_MENU = "open_modal_note_menu";
+export const CLOSE_MODAL_NOTE_MENU = "close_modal_note_menu";
