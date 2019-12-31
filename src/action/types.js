e4d6420e9c7cf991e6a4c7a1ecdd48b66b61be83
Kamil Nowak <kamilnowakx98@gmail.com>
2019-12-31 17:12:07 +0100

    add new action type

--- src/action/types.js
+++ src/action/types.js
@@ -3,5 +3,6 @@ export const DELETE_NOTE = "delete_note";
 export const DOWNLOAD_NOTES = "download_notes";
 export const DOWNLOAD_ID = "download_id";
 export const TEXT_NOTE = "text_note";
+export const CHANGE_COLOR_NOTE = "change_color_note";
 export const OPEN_MODAL_NOTE_MENU = "open_modal_note_menu";
 export const CLOSE_MODAL_NOTE_MENU = "close_modal_note_menu";
