14fe12b1d673eb62781734292acfb76afe8f4c56
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-02 01:08:53 +0100

    fix problem with backgroudColor in NoteElement

--- src/components/NoteElement.js
+++ src/components/NoteElement.js
@@ -3,6 +3,15 @@ import { View, Text, StyleSheet, TouchableNativeFeedback } from "react-native";
 import { screens, colors } from "../../setting.json";
 
 const NoteElement = ({ textItem, idItem, colorItem, onClick, onLongPress }) => {
+  const selectColor = (colors, colorItem) => {
+    console.log(colors, colorItem);
+    const colorIndex = colors.findIndex(color => color.name === colorItem);
+    if (colorIndex !== -1) {
+      return colors[colorIndex].value;
+    }
+    return colors[0].value;
+  };
+
   return (
     <TouchableNativeFeedback
       onPress={onClick}
@@ -11,7 +20,7 @@ const NoteElement = ({ textItem, idItem, colorItem, onClick, onLongPress }) => {
       <View
         style={{
           ...styles.noteView,
-          backgroundColor: colors.find(color => color.name === colorItem).value
+          backgroundColor: selectColor(colors, colorItem)
         }}
       >
         <Text style={styles.noteText}>{textItem}</Text>
