639e598bd24b89c540becae11b4538c7c499df45
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-04 17:13:22 +0100

    add new action type

--- src/action/types.js
+++ src/action/types.js
@@ -4,5 +4,7 @@ export const DOWNLOAD_NOTES = "download_notes";
 export const DOWNLOAD_ID = "download_id";
 export const TEXT_NOTE = "text_note";
 export const CHANGE_COLOR_NOTE = "change_color_note";
+export const CHANGE_FOCUS_MODAL_RADIO_BUTTON =
+  "change_focus_modal_radio_button";
 export const OPEN_MODAL_NOTE_MENU = "open_modal_note_menu";
 export const CLOSE_MODAL_NOTE_MENU = "close_modal_note_menu";
